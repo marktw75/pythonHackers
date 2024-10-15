42a811aed02d37a00db741dfbe82ca4146136c42
Mark Tsai <57780698+marktw75@users.noreply.github.com>
2024-10-16 01:39:00 +0800

    Create secondpythonscript.py

---
+++ secondpythonscript.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+"""
+ Listing 17-2: A series of data structures associated with variable
+"""
+ HackersAriseStringVariable = "Hackers-Arise Is the Best Place to Learn Hacking"
+ HackersAriseIntegerVariable = 12
+ HackersAriseFloatingPointVariable = 3.1415
+ HackersAriseList = [1, 2, 3, 4, 5, 6]
+ HackersAriseDictionary = {'name': 'OccupyTheWeb', 'value' : 27)
+ print(HackersAriseStringVariable)
+ print(HackersAriseIntegerVariable)
+ print(HackersAriseFloatingPointVariable)
